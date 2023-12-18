bff295f639849db24e7091d72009dff62cbee7ec
Mazin Fouad <m.fouad@live.de>
2023-12-18 18:23:19 +0100

    navbar created

--- src/app/landing-section/landing-section.component.ts
+++ src/app/landing-section/landing-section.component.ts
@@ -1,12 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-landing-section',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, NavbarComponent],
   templateUrl: './landing-section.component.html',
-  styleUrl: './landing-section.component.scss'
+  styleUrl: './landing-section.component.scss',
 })
-export class LandingSectionComponent {
-
-}
+export class LandingSectionComponent {}
